2aa73e03dec9b682c507b603243c93e3e8d7c1bc
Tuan Nguyen <nvtuan39202@gmail.com>
2023-10-27 08:10:32 +0700

    h

--- admin/all_menu.php
+++ admin/all_menu.php
@@ -255,7 +255,7 @@ session_start();
 																								
 																								
 																								<td><div class="col-md-3 col-lg-8 m-b-10">
-																								<center><img src="Res_img/dishes/' . $rows['img'] . '" class="img-responsive  radius" style="max-height:100px;max-width:150px;" /></center>
+																								<center><img src="Res_img/dishes/' . $rows['img'] . '" class="img-responsive  radius" style="height:100px;width:150px;object-fit:cover;" /></center>
 																								</div></td>
 																								
 																							
